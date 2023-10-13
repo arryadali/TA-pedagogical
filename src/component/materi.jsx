a7a30e3031ed742bf34dc3c77a27b5c081690c17
arryadali <arryadali@gmail.com>
2023-10-13 17:45:35 +0700

    third commit

--- src/component/materi.jsx
+++ src/component/materi.jsx
@@ -5,8 +5,25 @@ const Materi = () => {
   return (
     <section id='materi'>
         <Navbar/>
-        <div>
-            INI MATERI
+        <div className='mx-auto max-w-7xl'>
+            <p className='font-[poppins] text-[40px]'>MATERI</p>
+
+            <div className='bg-green-500 h-auto py-4'>
+              <div className='w-[95%] bg-red-500 mx-auto'>
+                MATERI PEMBELAJARAN KELAS 5 SD
+              </div>
+
+              <div className='flex w-[95%] mx-auto bg-orange-400 h-16 rounded-[5px] items-center my-4'>
+                <div className=''>
+                  <label className='font-[poppins] text-[20px]'>Matematika</label>
+                </div>
+
+                <div>
+                  <label className='bg-gray-500 rounded-[5px] font-semibold p-[2px] font-sans'>Bangun Ruang</label>
+                </div>
+              </div>
+            </div>
+
         </div>
     </section>
   )
--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -4,7 +4,39 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <section id='navbar'>
-        <div className='container'>
+      <div class="relative shadow bg-white">
+          <div class="absolute inset-0 bg-cover bg-center bg-no-repeat bg-fixed"></div>
+
+          <div class="w-full backdrop-blur-sm">
+              <div class="relative z-1 h-16 mx-auto px-5 max-w-7xl flex items-center justify-between text-white">
+                <img src="../asset/logo.png" alt="" className='w-[10%]'/>
+
+                  <ul class="flex items-center gap-12">
+                      <Link to={'/homepage'}>
+                        <li className='hover:text-[#1D809F] transition-colors text-black'>
+                          Dashboard
+                        </li>
+                      </Link>
+                      <Link to={'/biodatasiswa'}>
+                        <li className='hover:text-[#1D809F] transition-colors text-black'>
+                          Biodata Siswa
+                        </li>
+                      </Link>
+                      <Link to={'/materi'}>
+                        <li className='hover:text-[#1D809F] transition-colors text-black'>
+                          Materi
+                        </li>
+                      </Link>
+                      <Link to={'/soal'}>
+                        <li className='hover:text-[#1D809F] transition-colors text-black'>
+                          Soal
+                        </li>
+                      </Link>
+                  </ul>
+              </div>
+          </div>
+      </div>
+        {/* <div className='container'>
           <nav className='flex h-[70px] bg-yellow-400'>
             <div className='bg-red-500'>
               LOGO
@@ -26,7 +58,7 @@ const Navbar = () => {
               </ul>
             </div>
           </nav>
-        </div>
+        </div> */}
     </section>
   )
 }
