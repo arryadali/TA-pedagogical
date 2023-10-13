49096a97763db52cc045c51b41979489832ff934
arryadali <arryadali@gmail.com>
2023-10-13 08:44:20 +0700

    second commit

--- src/component/loginpage.jsx
+++ src/component/loginpage.jsx
@@ -4,7 +4,58 @@ import { Link } from 'react-router-dom';
 const Loginpage = () => {
   return (
     <section id='loginpage'>
-        <div className='flex w-[80%] m-auto h-auto'>
+        <div class="bg-gray-100 flex justify-center items-center h-screen">
+
+            <div class="w-1/2 h-screen hidden lg:block">
+                <div className='object-cover w-full h-full'></div>
+            </div>
+
+            <div class="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+                <h1 class="text-2xl font-semibold mb-4">Login</h1>
+
+                <form>
+
+                    <div class="mb-4">
+                        <label for="username" class="block text-gray-600">Username</label>
+                        <input 
+                        type="text"
+                        id='username'
+                        name='username'
+                        className='w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500'
+                        />
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="password" class="block text-gray-600">Password</label>
+                        <input 
+                        type="text"
+                        id='password'
+                        name='password'
+                        className='w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500'
+                        />
+                    </div>
+
+                    <Link to={'/homepage'}>
+                        <button 
+                        type="submit" 
+                        class="bg-[#1D809F]  text-white font-semibold rounded-md py-2 px-4 w-full">
+                            Masuk
+                        </button>
+                    </Link>
+                </form>
+
+            <div class="mt-6 text-center">
+                <p>Daftar sebagai administrator pada aplikasi ini?</p>
+                <Link to={'/registrasi'}>
+                    <p className='hover:underline text-blue-500'>Daftar Disini!</p>
+                </Link>
+            </div>
+            </div>
+        </div>
+
+
+
+        {/* <div className='flex w-[80%] m-auto h-auto'>
             <div className='bg-green-500 w-[50%]'>
                 a
             </div>
@@ -47,7 +98,7 @@ const Loginpage = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </div> */}
     </section>
   )
 }
